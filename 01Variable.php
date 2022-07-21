c792b2b7a6b9646de81c9f3fb8e4405fcb5bc12b
A. B. M. Noman <noman15-2839@diu.edu.bd>
2022-07-22 02:36:19 +0600

    Updated 0Variable.php

--- 01Variable.php
+++ 01Variable.php
@@ -1,2 +1,13 @@
 <?php
-echo"Hello World";
+$name = "Bangladesh"; //string
+$age = 51; //int
+
+echo "The name of my country is {$name}\n";
+
+var_dump($name); // Calculate Variable Type, Length and value
+var_dump($age);
+
+define('PI',3.1416); //Constant Variable, Unchangeable 
+echo "Value of pi = ".PI;
+echo "\n";
+echo constant('PI');
