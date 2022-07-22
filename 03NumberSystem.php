742fef97d1d16842d9798c708e69300dbf6e3c6b
A. B. M. Noman <noman15-2839@diu.edu.bd>
2022-07-23 02:04:37 +0600

    Number System Added

---
+++ 03NumberSystem.php
@@ -0,0 +1,13 @@
+<?php
+
+$d = 12; //Decimal Number
+$o = 012; //Octal Number
+$h = 0x12; //Hexadecimal Number
+
+printf("Numbers in Decimal form %d, %d and %d\n\n",$d, $o, $h);
+
+echo "Number Conversion: Decimal to another\n";
+
+printf("The Binary Equivalent of Decimal %d id %s\n",12,12);
+printf("The Octal Equivalent of Decimal %d is %o\n",12,12);
+printf("The Hexadecimal Equivalent of Decimal %d is %x\n",12,12);
