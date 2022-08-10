813029f20b26422212818a77400c69e2d891d009
A. B. M. Noman <noman15-2839@diu.edu.bd>
2022-08-10 21:40:07 +0600

    print updated

--- 04print.php
+++ 04print.php
@@ -1,8 +1,13 @@
 <?php
 $name = "Earth";
+$radius = 6400;
 echo $name;
+echo PHP_EOL;
+echo $radius;
 echo PHP_EOL; // End of Line, New Line
 
 var_dump($name); // printing variable type and string length with value except using echo.
+var_dump($radius);
+
 $uname = strtoupper($name);
 printf("We are living on %s", $uname); //String to upper case.
