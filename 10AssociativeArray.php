ba3e875df107636a5654e055d4cb90939eff7e5b
A. B. M. Noman <noman15-2839@diu.edu.bd>
2022-08-26 10:57:35 +0600

    Delimeter added in associative Array

--- 10AssociativeArray.php
+++ 10AssociativeArray.php
@@ -16,4 +16,25 @@ foreach($food as $key => $value){
     echo($key." = ".$value."\n");
 }
 
+echo PHP_EOL;
+
+
+//Separate element - Array to String
+echo "Separate element\n";
+$vegetable = explode(", ","potato, tomato, carrot, capsicum"); //delimeter
+var_dump($vegetable);
+echo $vegetable[2];
+echo PHP_EOL;
+
+
+//Multiple Delimeter
+echo "Separate element\n";
+$vegetable = preg_split("/(, |,)/","potato, tomato, carrot,capsicum"); //Multiple delimeter
+print_r($vegetable);
+echo PHP_EOL;
+
+
+//Separate element - String to Array
+$vegetableString = join(", ",$vegetable);
+echo $vegetableString;
 ?>
