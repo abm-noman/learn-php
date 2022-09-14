d4bb207e62d2707450ee19bea0c54749a6d6659b
A. B. M. Noman <noman15-2839@diu.edu.bd>
2022-09-15 01:49:33 +0600

    String to ASCII

---
+++ 14Ascii.php
@@ -0,0 +1,15 @@
+<?php
+//ASCII value of Alphabet , String to Ascii
+
+echo ord('A');
+echo PHP_EOL;
+
+echo ord('N');
+echo PHP_EOL;
+
+echo ord('a');
+echo PHP_EOL;
+
+//ASCII value of Alphabet , ASCII to String
+
+echo chr('97');
