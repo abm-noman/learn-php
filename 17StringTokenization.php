56ff6a14149fd87fc267c01ab867921a51e3139d
A. B. M. Noman <noman15-2839@diu.edu.bd>
2022-09-15 19:45:38 +0600

    Tokenization Updated

--- 17StringTokenization.php
+++ 17StringTokenization.php
@@ -13,3 +13,16 @@ echo PHP_EOL;
 // Separate every character from a string
 $parts2 = str_split($string);
 print_r($parts2);
+
+//Separate String using multiple delimeter
+$string2 = "Hello World, How are you?";
+$parts3 = strtok($string2, " ,");
+while ($parts3 !== false) {
+    echo $parts3."\n";
+    $parts3 = strtok(" ?,");
+}
+
+echo PHP_EOL;
+$string3 = "A quick Brown fox jumps over the lazy dog";
+$parts4 = preg_split("/ |,/", $string3);
+print_r($parts4);
