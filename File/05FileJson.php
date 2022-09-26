9e68108e37fa068c09db6aed7ea5342fe611f422
A. B. M. Noman <noman15-2839@diu.edu.bd>
2022-09-26 12:42:17 +0600

    Crud Operation

---
+++ File/05FileJson.php
@@ -0,0 +1,37 @@
+<?php
+
+$filename = "D:\Workspace\PHP\File\data\File4.txt";
+
+$students = array(
+    array(
+        'fname'=>'Abm',
+        'lname'=>'Noman',
+        'age'=>12,
+        'class'=>7,
+        'roll'=>1
+    ),
+
+    array(
+        'fname'=>'Abdullah',
+        'lname'=>'Moin',
+        'age'=>11,
+        'class'=>7,
+        'roll'=>2
+    ),
+
+    array(
+        'fname'=>'Sabbir',
+        'lname'=>'Hossain',
+        'age'=>13,
+        'class'=>7,
+        'roll'=>3
+    ),
+
+    array(
+        'fname'=>'Minhazul',
+        'lname'=>'Alam',
+        'age'=>12,
+        'class'=>7,
+        'roll'=>4
+    ),
+    );
---
+++ crud/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Index</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+    <style>
+        body{
+            margin-top:30px;
+        }
+    </style>
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="column column-60 column-offset-20">
+                <h1>CRUD Operation</h1>
+                <p>A sample project to perform CRUD(Create Read Update Delete) operations using plain files and PHP</p>
+                <?php include_once ('inc/templates/nav.php');?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
