3b01bf06b5b7c65c05c459fb00f2d2c149e31ef4
A. B. M. Noman <noman15-2839@diu.edu.bd>
2022-11-02 23:54:03 +0600

    Interface Iterator Count

---
+++ oop/foundation/oop12_Interface_Iterator_count.php
@@ -0,0 +1,39 @@
+<?php
+
+class DistrictCollection implements IteratorAggregate,Countable{
+    private $districts;
+    function __construct()
+    {
+        $this->districts = array();   
+    }
+
+    function add($district){
+        array_push($this->districts,$district);
+    }
+
+    // function getDistrict(){
+    //     return $this->districts;
+    // }
+
+    function getIterator(): Traversable
+    {
+        return new ArrayIterator($this->districts);
+    }
+
+    function count(){
+        return count($this->districts);
+    }
+}
+
+
+$districts = new DistrictCollection;
+$districts->add("Mymensingh");
+$districts->add("Dhaka");
+$districts->add("Chittagong");
+$districts->add("Sylhet");
+$districts->add("Rangpur");
+$districts->add("Rajshahi");
+
+
+
+echo count($districts);
