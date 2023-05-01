fe51f8b8e7d0bf32e5036d21d2c4d39568ea87cc
abm-noman <noman15-2839@diu.edu.bd>
2023-05-01 15:23:14 +0600

    String Replace

---
+++ oop/advance/methodChain01.php
@@ -0,0 +1,44 @@
+<?php
+    class StringUtility{
+        private $string;
+        private $search;
+       
+        function __construct($string)
+        {
+            $this->string = $string;
+        }
+
+        function search($string){
+            $this->search = $string;
+            return $this;
+        }
+
+        function replace($string){
+            if(!isset($this->search)){
+                throw new Exception("Nothing to replace");
+            }
+            else{
+                $this->string = str_replace($this->search, $string, $this->string);
+                $this->search = "";
+                return $this;
+            }
+        }
+
+        function upperCase(){
+            $this->string = strtoupper($this->string);
+            return $this;
+        }
+
+        function lowerCase(){
+            $this->string = strtolower($this->string);
+            return $this;
+        }
+
+        function print(){
+            echo $this->string;
+        }
+    }
+
+    $s = new StringUtility("Hello World");
+    $s->search("World")->replace("Earth")->upperCase()->print();
+?>
