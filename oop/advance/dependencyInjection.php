e5c50f79a1a560087c5495517857b3d14774ca23
abm-noman <noman15-2839@diu.edu.bd>
2023-05-01 18:26:38 +0600

    Dependency Injection

---
+++ oop/advance/dependencyInjection.php
@@ -0,0 +1,33 @@
+<?php
+//Dependency Injection or Inversion of Control
+
+    interface BaseStudent{
+        function displayName();
+    }
+
+    class Student implements BaseStudent{
+        private $name;
+        function __construct($name)
+        {
+            $this->name = $name;
+        }
+        function displayName()
+        {
+            echo "Hello from ".$this->name;
+        }
+    }
+
+    class StudentManager{
+        function introduceStudent($name){
+            $st = new Student($name);
+            $st->displayName();
+        }
+
+    }
+    
+    $sm = new StudentManager();
+    $sm->introduceStudent("Noman");
+
+
+
+?>
